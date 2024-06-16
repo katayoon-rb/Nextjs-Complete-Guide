97b8cbf7e25a4f2b96ce4f50c9c63dfb76ce3b60
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-06-16 22:20:14 +0330

    Add files via upload

---
+++ 1-Next-Level-Food/components/main-header/main-header.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-bg";
+import NavLink from "./nav-link";
+
+export default function MainHeader() {
+  return (
+    <div>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href='/'>
+          <Image priority src={logoImg} alt='A plate with food on it' />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href='/meals'>Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href='/community'>Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
