fc2b7dd9934c99b398f60013554b34f62a052c60
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-06-20 22:13:23 +0330

    Add files via upload

---
+++ 5-React-Meetups/pages/index.js
@@ -0,0 +1,39 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+import MeetupList from "../components/meetups/MeetupList";
+
+export default function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name='description'
+          content='Browse a huge list of highly active React meetups!'
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(process.env.NEXT_MONGODB_URI);
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
